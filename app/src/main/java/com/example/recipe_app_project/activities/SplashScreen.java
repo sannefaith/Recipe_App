87d7fd242607650505e849fd0df175dfcaf0859c
sannefaith <odhiambo.faith@strathmore.edu>
2021-08-14 17:44:23 +0300

    Create SplashScreen.java

---
+++ app/src/main/java/com/example/recipe_app_project/activities/SplashScreen.java
@@ -0,0 +1,17 @@
+package com.example.recipe_app_project.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.recipe_app_project.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
